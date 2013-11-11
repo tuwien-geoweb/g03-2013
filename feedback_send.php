203a24a4e92a2be5ad80c1788e596a037632f0e2
cobererlacher <c_obererlacher@gmx.at>
2013-11-11 20:52:29 +0100

    Create feedback_send.php
    Feedback_send.php - angepasst an Speicherort app

---
+++ feedback_send.php
@@ -0,0 +1,53 @@
+<?php
+  $name = $_REQUEST['name'];  // $_REQUEST enthält die Benutzerangaben
+  $email = $_REQUEST['email'];
+  $message = $_REQUEST['message'] ;
+
+  if (isset($_REQUEST['geschlecht'])) 
+     {$anrede=$_REQUEST['geschlecht'];} // Frau/Herr
+  else 
+     {$anrede=" ";}
+     
+  if (isset($_REQUEST['team'])) 
+     {$team="geoweb-Mitglied";
+      $teamflag=1;} 
+  else 
+     {$team="geoweb-extern";
+      $teamflag=0;}
+
+  // Funktion mail(adress,subject,message,header) für Versenden per Mail
+  mail( "jumbostone@hotmail.com",
+        "geoweb: Feedback Formular", 
+        "Gesendet von ".$anrede." ".$name." (".$team."): ".$message, 
+        "From: $email" ) 
+       OR DIE("Fehler: Feedback nicht gesendet.");  
+
+  // Daten zusätzlich in Datenbank speichern (siehe auch PhpSql-Abschnitt)
+  // Tabelle feedback muss in Datenbank angelegt sein 
+  // (Beispiel-Tabelle siehe geoweb/2013/ifip/ifip_db.sqlite-Datenbank)
+  
+  include 'geoweb_db_open.php'; // ifip_db.sqlite-Datenbank öffnen
+  
+  // Daten in Tabelle feedback einfügen mit SQL-Befehl 
+  // INSERT INTO <tabelle> (felder, ...) VALUES (werte, ...) 
+  // Die Werte sind bei Textfelder in (einfache) Hochkomma zu setzen, 
+  // bei Zahlen ohne Hochkomma (hier nur bei teamflag)
+  // SQL-String zusammensetzen
+	
+  $sql = "INSERT INTO feedback (f_name,f_mail,f_anrede,f_msg,f_geoweb,f_datum)";
+  $sql = $sql . " VALUES ('" . $name . "','" . $email . "','" . $anrede . 
+         "','" . $message . "'," . $teamflag . ",'" . date("d-m-Y") . "')";
+         
+
+  // SQL-String an Datenbank-Server schicken (Beispiel SQLite-Datenbank: 
+  $db->exec($sql) or die ('Fehler beim Speichern: '.$db->lastErrorMsg());
+  
+  include 'geoweb_db_close.php'; // Datenbank schließen
+
+echo "Danke für das Feedback!<br />Die Daten wurden per Mail übermittelt".
+     " und in einer Datenbank gespeichert!";
+     
+/* Alternativ: Aufruf einer Html-Seite für Danksagung */ 
+/* header( "Location: http://xxx.yyy/feedback_thank.htm" );exit; */
+
+?> 
