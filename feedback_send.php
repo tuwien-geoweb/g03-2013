8139e7de73a3226efe87078823c9da003bab298d
cobererlacher <c_obererlacher@gmx.at>
2013-11-12 10:23:20 +0100

    Update feedback_send.php

--- feedback_send.php
+++ feedback_send.php
@@ -38,7 +38,7 @@
 	
   $sql = "INSERT INTO feedback (f_name,f_mail,f_anrede,f_msg,f_geoweb,f_datum, geom)";
   $sql = $sql . " VALUES ('" . $name . "','" . $email . "','" . $anrede . 
-         "','" . $message . "'," . $teamflag . ",'" . date("d-m-Y") . "'"', GeomFromText('5.123123 10.123121234', 4326))");
+         "','" . $message . "'," . $teamflag . ",'" . date("d-m-Y") . "', '" . $longitutde . "')";
          
 
   // SQL-String an Datenbank-Server schicken (Beispiel SQLite-Datenbank: 
