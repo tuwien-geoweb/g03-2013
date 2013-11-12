0d3a3b58526f4ecd9e752f133fc95ffdcd2ce975
cobererlacher <c_obererlacher@gmx.at>
2013-11-12 11:36:34 +0100

    Update feedback_formular.php

--- feedback_formular.php
+++ feedback_formular.php
@@ -59,8 +59,8 @@
                                  <td><input type="text" name="email" size="50" /></td>
                          </tr>
                          <tr><td>Ihre Koordinaten </td>
-                                 <td><input type="text" name="longitude" size="50" /></td>
-                                 <td><input type="text" name="latitude" size="50" /></td>
+                                 <td><input type="text" name="longitude" size="25" /></td>
+                                 <td><input type="text" name="latitude" size="25" /></td>
                           </tr>
                  </table>
                 Feedback: <br />
