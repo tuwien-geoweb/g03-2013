981c19793643ca547d6fe94a5246b37e6c8b5d1d
cobererlacher <c_obererlacher@gmx.at>
2013-11-12 11:32:28 +0100

    Update feedback_formular.php
    bootstrap adaptiert

--- feedback_formular.php
+++ feedback_formular.php
@@ -8,6 +8,14 @@
 <meta name="keywords" content="Wohnstandortsuche" />
 <meta name="description" content="Wohnstandortsuche Gruppe 3" />
 <link href="../style.css" rel="stylesheet" type="text/css" />
+  
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="grid.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="ol.css">
 </head>
 <body>
 
@@ -29,52 +37,84 @@
         </div>
 </div>
 
+  <style> .span1 {
+        height: 400px;
+    }
+      
+   </style>
 
-<div id="page">
+<div class="container">
+    
+      <div class="row">
+         <div id="text" class="col-md-6">
+          <h3>Formular</h3>
+                 <form method="POST" action="feedback_send.php">
+                 <input type="radio" name="geschlecht" value="Frau"/> Frau
+                 <input type="radio" name="geschlecht" value="Herr"/> Herr<br />
+                 <table>
+                         <tr><td>Name:</td>
+                                 <td><input type="text" name="name" size="50" /></td>
+                         </tr>
+                         <tr><td>E-Mail: </td>
+                                 <td><input type="text" name="email" size="50" /></td>
+                         </tr>
+                         <tr><td>Ihre Koordinaten </td>
+                                 <td><input type="text" name="longitude" size="50" /></td>
+                                 <td><input type="text" name="latitude" size="50" /></td>
+                          </tr>
+                 </table>
+                Feedback: <br />
+                         <textarea name="message" rows="10" cols="50"></textarea>
+                         <br />
+                         <input type="checkbox" name="team" checked="checked" value="ON" />
+                         Ich bin Mitglied des geoweb-Teams <br /><br />
+                         <input type="submit" value="Abschicken">
+                         <input type="reset" value="Zurücksetzen"> <br /><br />
+                         Ihr Feedback wird per E-Mail an die Autoren/innen zugestellt<br>
+                         und in der Projekt-Datenbank gespeichert.<br />
+                        </form>
+                
+                <p>geoweb.m10 (JB), Beispiel ausgehend von<br />
+                <a href="http://www.thesitewizard.com/archive/feedbackphp.shtml"
+                target="_blank"> PHP Tutorial: Feedback Form Script</a> </p>
+         </div>
 
-        <div id="bodystyle">
-        <h1 class="title">Feedback Formular</h1>
-
-        <form method="POST" action="feedback_send.php">
-         <input type="radio" name="geschlecht" value="Frau"/> Frau
-         <input type="radio" name="geschlecht" value="Herr"/> Herr<br />
-         <table>
-         <tr><td>Name:</td>
-         <td><input type="text" name="name" size="50" /></td>
-         </tr>
-         <tr><td>E-Mail: </td>
-         <td><input type="text" name="email" size="50" /></td>
-         </tr>
-         <tr><td>Ihre Koordinaten </td>
-         <td><input type="text" name="longitude" size="50" /></td>
-         <td><input type="text" name="latitude" size="50" /></td>
-         </tr>
-         </table>
-         Feedback: <br />
-         <textarea name="message" rows="10" cols="50"></textarea>
-         <br />
-         <input type="checkbox" name="team" checked="checked" value="ON" />
-         Ich bin Mitglied des geoweb-Teams <br /><br />
-         <input type="submit" value="Abschicken">
-         <input type="reset" value="Zurücksetzen"> <br /><br />
-         Ihr Feedback wird per E-Mail an die Autoren/innen zugestellt<br>
-         und in der Projekt-Datenbank gespeichert.<br />
-        </form>
-
-<p>geoweb.m10 (JB), Beispiel ausgehend von<br />
-<a href="http://www.thesitewizard.com/archive/feedbackphp.shtml"
-target="_blank"> PHP Tutorial: Feedback Form Script</a> </p>
-        </div>
-
-                <div style="clear: both;">&nbsp;</div>
+         <div class="col-md-6">
+                  <div id="map" class="span1">
+                  </div>
+         </div>
+   </div>        <div style="clear: both;">&nbsp;</div>
 </div>
 
+
+    <script src="ol.js"></script><script type="text/javascript">
+      var map = new ol.Map({
+      target: 'map',
+      renderer: ol.RendererHint.CANVAS,
+      view: new ol.View2D({
+        center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 11
+      }),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.MapQuestOpenAerial()
+        }),
+        new ol.layer.Vector({
+          source: new ol.source.Vector({
+            url: 'locations.geojson',
+            parser: new ol.parser.GeoJSON()
+          })
+        })
+      ]
+    });
+    </script>
 
 <div id="unten">
         <div id="unten1">
         <p id="fusszeile">© 2013 Gruppe 3, Webbasierte Geoinformation im Planungsprozess</a></p>
         </div>
 </div>
+
 
 </body>
 </html>
