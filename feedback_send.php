df49cefe08d1a2b6871b5653a742ac1a0fafb1b5
jumbostone <jumbostone@hotmail.com>
2013-11-18 13:07:20 +0100

    Update feedback_send.php

--- feedback_send.php
+++ feedback_send.php
@@ -4,8 +4,8 @@
   $message = $_REQUEST['message'] ;
   $longitude = $_REQUEST['longitude'] ;
   $latitude = $_REQUEST['latitude'] ;
-  $lon = ROUND($longitude, 4) ;
-  $lat = ROUND($latitude, 4) ;
+  $lon = ROUND($longitude, length[4]) ;
+  $lat = ROUND($latitude, length[4]) ;
 
   if (isset($_REQUEST['geschlecht'])) 
      {$anrede=$_REQUEST['geschlecht'];} // Frau/Herr
