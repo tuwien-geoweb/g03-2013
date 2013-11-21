6f4e5557d66ba28b569727801b9acf6d62356c02
cobererlacher <c_obererlacher@gmx.at>
2013-11-21 13:18:30 +0100

    Update censusmap-simple.js

--- censusmap-simple.js
+++ censusmap-simple.js
@@ -5,7 +5,7 @@ var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
 var wmsLayer = new ol.layer.Image({
   source: new ol.source.ImageWMS({
     url: '/geoserver/wms',
-    params: {'LAYERS': 'g02_2013:g03_2013_normalized'}
+    params: {'LAYERS': 'g03_2013:g03_2013_normalized'}
   }),
   opacity: 0.6
 });
