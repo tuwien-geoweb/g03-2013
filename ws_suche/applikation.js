d27737d6a63a68581e1f5317c1b373a1e36495ea
cobererlacher <c_obererlacher@gmx.at>
2013-12-05 11:50:14 +0100

    Update applikation.js

--- ws_suche/applikation.js
+++ ws_suche/applikation.js
@@ -18,7 +18,7 @@ olMap = new ol.Map({
   view: new ol.View2D({
     center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
     zoom: 12,
-    maxZoom: 18
+    maxZoom: 20
        })
 });
 
